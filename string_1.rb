08e8829455c6bd8e4a5ef1915d8417b656f252a3
Toropon <duck7quick@gmail.com>
2020-11-02 00:01:12 +0900

    add unpack~succ

--- string_1.rb
+++ string_1.rb
@@ -56,3 +56,47 @@ p a.rjust(20, "=")    #=> "=================abc"
 a = "abc\tdef\tghi\n"
 puts a        #=> abc	def	ghi
 puts a.dump   #=> "abc\tdef\tghi\n"
+
+=begin
+  文字列をアンパックする
+  unpackメソッドはArray#packメソッドでパックされた文字列を、
+  指定したテンプレートに従ってアンパックする
+  unpackはMIMEエンコードされた文字列のデコードや、バイナリデータから数値変換などに利用される
+=end
+
+p '440r440T4408'.unpack('m')              #=> ["\xE3\x8D+\xE3\x8D\x13\xE3\x8D<"]
+# アンパックされた文字列のエンコーディングはASCII-8BITになる
+p '440r440T4408'.unpack('m')              #=> ["\xE3\x8D+\xE3\x8D\x13\xE3\x8D<"]
+a = p '440r440T4408'.unpack('m').first    #=> "\xE3\x8D+\xE3\x8D\x13\xE3\x8D<"
+p a.force_encoding('UTF-8')               #=> "ルビー"
+
+=begin
+  文字列内での検索
+
+  include?
+  index
+  rindex
+  match
+  scan
+
+  include?メソッドは指定された文字列が含まれているときにtrueを返す
+  indexメソッドは指定された文字や文字コードの数値、正規表現などのパターンを指定した位置から
+  右方向に検索して最初に見つかった位置を返す
+  rindexメソッドはindexメソッドとは逆に左方向に検索する
+  matchメソッドは指定された正規表現によるマッチングを行い、マッチした場合にはMatchDataオブジェクトを返す
+  scanメソッドは指定された正規表現にマッチした部分文字列の配列を返す
+  またscanメソッドはブロックを渡すことができる
+=end
+
+p "abcdefg".include?("abc")   #=> true
+p "abcdefg".index("cd")       #=> 2
+p "abcdefg".match(/[c-f]/)    #=> #<MatchData "c">
+p "abcdefg".scan(/[c-d]/)     #=> ["c", "d"]
+
+# 次の文字列を求める(succ, succ!, next, next!)
+p "abc123".succ     #=> "abc124"
+p "123abc".succ     #=> "123abd"
+p "123xyz".succ     #=> "123xza"
+p "99999".succ     #=> "100000"
+p "zzzzz".succ     #=> "aaaaaa"
+p "ZZZZZ".succ     #=> "AAAAAA"
